1ba5f5a18b2c47ee2d3b55e33a36b1d01eaa077a
saikiran <aakulasaikiran>
2018-07-17 09:51:59 +0530

    xyz

--- Application25/src/App2.java
+++ Application25/src/App2.java
@@ -3,21 +3,17 @@ public class App2 {
 
 	public static void main(String[] args) {
 
-
-		String[] s= {"2","4","6","8","10","12","14","workspace_1","workspace2","987"};
-
-
 		
-
 	
 
 	
-
-
 		
+
+		String[] s = { "2", "4", "6", "8", "10", "12", "14", "workspace_1", "workspace2", "abc","xyz","123" };
+
 		for (int i = 0; i < s.length; i++) {
 			System.out.print(s[i] + " ");
-}
+		}
 	}
 
 }
