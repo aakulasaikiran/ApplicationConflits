abec15f2f8bbad6be5ca0e46d4b5f72776389212
saikiran <aakulasaikiran>
2018-07-17 09:21:07 +0530

    added 14

--- Application25/src/App2.java
+++ Application25/src/App2.java
@@ -2,7 +2,7 @@
 public class App2 {
 
 	public static void main(String[] args) {
-		String[] s= {"2","4","6","8","10","12"};
+		String[] s= {"2","4","6","8","10","12","14"};
 		for(int i=0;i<s.length;i++) {
 		System.out.print(s[i]+" ");	
 		}
