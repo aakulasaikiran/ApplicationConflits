9061c3ac57a21c071edab8795c0d3eb550f81e5c
saikiran <aakulasaikiran>
2018-07-17 09:39:52 +0530

    add xyz

--- Application25/src/App2.java
+++ Application25/src/App2.java
@@ -3,7 +3,7 @@ public class App2 {
 
 	public static void main(String[] args) {
 
-		String[] s = { "2", "4", "6", "8", "10", "12", "14", "workspace_1", "workspace2", "abc" };
+		String[] s = { "2", "4", "6", "8", "10", "12", "14", "workspace_1", "workspace2", "abc","xyz" };
 
 		for (int i = 0; i < s.length; i++) {
 			System.out.print(s[i] + " ");
