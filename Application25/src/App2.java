0b14d00ccdf961fc5549fa0c16f3294d8d73b8b1
saikiran <aakulasaikiran>
2018-07-17 11:30:39 +0530

    remove ,

--- Application25/src/App2.java
+++ Application25/src/App2.java
@@ -9,7 +9,7 @@ public class App2 {
 	
 		
 
-		String[] s = { "2", "4", "6", "8", "10", "12", "14",};
+		String[] s = { "2", "4", "6", "8", "10", "12", "14"};
 
 		for (int i = 0; i < s.length; i++) {
 			System.out.print(s[i] + " ");
