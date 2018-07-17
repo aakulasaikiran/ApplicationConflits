6724e46f9761342db20c49afc6fa070cc6f2c2d9
saikiran <aakulasaikiran>
2018-07-17 09:14:32 +0530

    commit1

---
+++ Application25/src/App2.java
@@ -0,0 +1,11 @@
+
+public class App2 {
+
+	public static void main(String[] args) {
+		String[] s= {"2","4","6","8","10"};
+		for(int i=0;i<s.length;i++) {
+		System.out.print(s[i]+" ");	
+		}
+	}
+
+}
